dacb8da0a57f922c2c054caca593f30db61acf2b
Ainhooaa <amartinez574@ikasle.ehu.eus>
2026-03-05 13:06:09 +0100

    Probando

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import React from "react";
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1> Bienvenidos a la Pizzería Masa Madre</h1>
+      <h1> Bienvenidos a la Pizzería Masa Madrey</h1>
       <p>Próximamente podrás ver nuestro menú y hacer pedidos online.</p>
     </div>
   );
 }
-
 export default App;
