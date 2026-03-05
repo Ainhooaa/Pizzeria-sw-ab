bf6d2869e5cd08f9f70a209afc35f6014e5384ed
Ainhooaa <amartinez574@ikasle.ehu.eus>
2026-03-05 11:16:45 +0100

    Proyecto inicial Pizzería SW con Vite + React + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1> Bienvenidos a la Pizzería Masa Madre</h1>
+      <p>Próximamente podrás ver nuestro menú y hacer pedidos online.</p>
+    </div>
+  );
+}
+
+export default App;
